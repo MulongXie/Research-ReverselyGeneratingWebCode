e6efea56858eb7542c41046c6654d4c7260d1746
Mulong <dsh15325@163.com>
2019-05-24 11:29:02 +1000

    crawl top 500 websites

--- code/MODULE/webcrawler/crawl_test3.py
+++ code/MODULE/webcrawler/crawl_test3.py
@@ -1,6 +1,17 @@
-s = set()
+from bs4 import BeautifulSoup as bs
+import urllib.request as url
+import pandas as pd
 
-s.add('a')
-s.add('b')
+resp = url.urlopen('file:D:\git_file\github\doing\Research-ReverselyGeneratingWebCode\code\MODULE\webcrawler\links.html')
+content = resp.read()
 
-print(len(s))
+soup = bs(content, 'html.parser')
+links = soup.find_all('a')
+
+csv = pd.DataFrame(columns=['link'])
+
+for i, l in enumerate(links):
+    csv.loc[i] = l['href']
+
+print(csv)
+csv.to_csv('preset_links_500.csv')
