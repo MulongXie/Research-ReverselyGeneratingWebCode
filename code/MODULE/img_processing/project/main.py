6fa85d18820bc35813b3991201be73e2774cd464
Mulong <dsh15325@163.com>
2019-06-21 19:51:08 +1000

    add more test cases

--- code/MODULE/img_processing/project/main.py
+++ code/MODULE/img_processing/project/main.py
@@ -2,15 +2,17 @@ import ip_detection as det
 import ip_preprocessing as pre
 
 import cv2
-import numpy as np
 import time
 
+start = time.clock()
 
-org, gray = pre.read_img('2.png', [600, 1500])
+org, gray = pre.read_img('1.png', (1500, 2200))  # cut out partial img
 binary = pre.preprocess(gray)
 boundary_all, boundary_rec = det.rectangle_detection(binary)
 corners = det.get_corner(boundary_rec)
 det.draw_bounding_box(corners, org)
+
+print(time.clock() - start)  # running time
 
 cv2.imshow('org', org)
 cv2.imshow('binary', binary)
