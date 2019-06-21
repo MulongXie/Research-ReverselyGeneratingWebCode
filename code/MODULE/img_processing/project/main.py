76200fcfd90fd2fecee87ee4d849690b8164e0b7
Mulong <dsh15325@163.com>
2019-06-21 20:42:16 +1000

    write out result

--- code/MODULE/img_processing/project/main.py
+++ code/MODULE/img_processing/project/main.py
@@ -6,17 +6,21 @@ import time
 
 start = time.clock()
 
-org, gray = pre.read_img('1.png', (0, 600))  # cut out partial img
+org, gray = pre.read_img('3.png', (0, -1))  # cut out partial img
 binary = pre.preprocess(gray)
 boundary_all, boundary_rec = det.rectangle_detection(binary)
 corners = det.get_corner(boundary_rec)
 
-bounding_broad = det.draw_bounding_box(corners, org)
-boundary_broad = det.draw_boundary(boundary_all, org.shape)
+bounding_drawn = det.draw_bounding_box(corners, org)
+boundary_drawn = det.draw_boundary(boundary_all, org.shape)
 
 print(time.clock() - start)  # running time
 
-cv2.imshow('org', bounding_broad)
-cv2.imshow('boundary', boundary_broad)
-cv2.imshow('gradient', binary)
-cv2.waitKey(0)
+cv2.imwrite('bounding.png', bounding_drawn)
+cv2.imwrite('boundary.png', boundary_drawn)
+cv2.imwrite('gradient.png', binary)
+
+# cv2.imshow('org', bounding_broad)
+# cv2.imshow('boundary', boundary_broad)
+# cv2.imshow('gradient', binary)
+# cv2.waitKey(0)
