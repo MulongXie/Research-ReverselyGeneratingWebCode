bd1b4f2df63f940e8162b7fc23216a0c3e9d0c63
Mulong <dsh15325@163.com>
2019-07-06 18:14:20 +1000

    add horizontal scan

--- code/MODULE/img_processing/project_test/ip_detection.py
+++ code/MODULE/img_processing/project_test/ip_detection.py
@@ -128,33 +128,33 @@ def rec_compress(binary, corners, max_thickness=3):
         width = y_max - y_min - 2 * max_thickness
         height = x_max - x_min - 2 * max_thickness
 
-        # scan vertically
         # line: count_divide_column > 0.9
         # background: count_divide_column < 0.1
+        # scan horizontally
+        for x in range(x_min + max_thickness, x_max - max_thickness):
+            count_divide_column = np.sum(binary[x, y_min+max_thickness: y_max-max_thickness])/255/width
+            count_divide_column_pre = np.sum(binary[x-max_thickness, y_min+max_thickness: y_max-max_thickness])/255/width
+            # left inner border: current column is line (all one) + previous column is background (all zero)
+            if count_divide_column > 0.9 and count_divide_column_pre == 0:
+                if x_max - x > max_thickness and x - x_min > max_thickness:
+                    x_min = x
+            # right inner border: current column is background (all zero) + previous column is
+            elif count_divide_column == 0 and count_divide_column_pre > 0.9:
+                if x - x_min > max_thickness and x_max - x > max_thickness:
+                    x_max = x
+                    
+        # scan vertically
         for y in range(y_min + max_thickness, y_max - max_thickness):
             count_divide_column = np.sum(binary[x_min+max_thickness: x_max-max_thickness, y])/255/height
             count_divide_column_pre = np.sum(binary[x_min+max_thickness: x_max-max_thickness, y-max_thickness])/255/height
-
             # left inner border: current column is line (all one) + previous column is background (all zero)
             if count_divide_column > 0.9 and count_divide_column_pre == 0:
                 if y_max - y > max_thickness and y - y_min > max_thickness:
                     y_min = y
-                    y += max_thickness
-                    draw_coners.pop()
-                    draw_coners.append(((y_min, x_min), (y_min, x_max)))
-                    broad = draw.draw_bounding_box(draw_coners, broad, (255, 0, 0))
-                    cv2.imshow('broad', broad)
-                    cv2.waitKey(0)
             # right inner border: current column is background (all zero) + previous column is
             elif count_divide_column == 0 and count_divide_column_pre > 0.9:
                 if y - y_min > max_thickness and y_max - y > max_thickness:
                     y_max = y
-                    y += max_thickness
-                    draw_coners.pop()
-                    draw_coners.append(((y_max, x_min), (y_max, x_max)))
-                    broad = draw.draw_bounding_box(draw_coners, broad, (0, 255, 0))
-                    cv2.imshow('broad', broad)
-                    cv2.waitKey(0)
 
         compressed_corners.append(((y_min, x_min), (y_max, x_max)))
     return compressed_corners
