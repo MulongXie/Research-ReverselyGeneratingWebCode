0bc3cb1456229ac0807ab98c54312e9d1279257a
Mulong <dsh15325@163.com>
2019-07-25 14:10:47 +1000

    add img segment

--- code/IMG2CODE/ip_main.py
+++ code/IMG2CODE/ip_main.py
@@ -1,6 +1,7 @@
 import ip_detection as det
 import ip_preprocessing as pre
 import ip_draw as draw
+import ip_segment as seg
 import file_utils as file
 from CONFIG import Config
 
@@ -17,7 +18,7 @@ is_show = False
 start = time.clock()
 
 # pre-processing: gray, gradient, binary
-org, gray = pre.read_img('input/2.png', (0, 3000))  # cut out partial img
+org, gray = pre.read_img('input/1.png', (0, 3000))  # cut out partial img
 binary = pre.preprocess(gray, 1)
 
 # processing: get connected areas -> get boundary -> rectangle check -> get corner of boundaries -> img or frame check -> refine img component
@@ -26,10 +27,13 @@ corners_rec = det.get_corner(boundary_rec)
 corners_block, corners_img = det.block_or_img(binary, corners_rec, C.THRESHOLD_MAX_BORDER_THICKNESS)
 corners_img = det.img_refine2(corners_img, C.THRESHOLD_MAX_EDGE_RATIO)
 
+# remove img elements and segment into smaller size
+img_clean = draw.draw_bounding_box(corners_img, org, (255, 255, 255), -1)
+seg.segment_img(img_clean, 600, 'output/segment')
+
 # draw results
 draw_bounding = draw.draw_bounding_box(corners_block, org, (0, 255, 0))
 draw_bounding = draw.draw_bounding_box(corners_img, draw_bounding, (0, 0, 255))
-wireframe = draw.draw_bounding_box(corners_img, org, (119, 136, 153), -1)
 draw_boundary = draw.draw_boundary(boundary_rec, org.shape)
 # save results
 if is_save:
@@ -37,7 +41,7 @@ if is_save:
     cv2.imwrite('output/labeled.png', draw_bounding)
     cv2.imwrite('output/boundary.png', draw_boundary)
     cv2.imwrite('output/gradient.png', binary)
-    cv2.imwrite('output/wireframe.png', wireframe)
+    cv2.imwrite('output/clean.png', img_clean)
     file.save_corners('output/corners.csv', corners_block, 'div')
     file.save_corners('output/corners.csv', corners_img, 'img', False)
 
@@ -46,7 +50,7 @@ if is_show:
     cv2.imshow('org', draw_bounding)
     cv2.imshow('boundary', draw_boundary)
     cv2.imshow('gradient', binary)
-    cv2.imshow('wireframe', wireframe)
+    cv2.imshow('clean', img_clean)
     cv2.waitKey(0)
 
 print(time.clock() - start)  # running time
