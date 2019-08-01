a82967febd1164abe33ddeabf480cd54aad792cc
Mulong <dsh15325@163.com>
2019-08-01 17:49:32 +1000

    start irregular and potential elements recognition

---
+++ code/MODULE/img_processing_project/CONFIG.py
@@ -0,0 +1,20 @@
+class Config:
+
+    def __init__(self):
+        self.ROOT_LABEL = "E:\Mulong\Datasets\dataset_webpage\data1\label_rec"
+        self.ROOT_IMG_ORG = "E:\Mulong\Datasets\dataset_webpage\data1\img_org"
+        self.ROOT_IMG_DRAWN = "E:\Mulong\Datasets\dataset_webpage\data1\img_drawn"
+        self.ROOT_IMG_GRADIENT = "E:\Mulong\Datasets\dataset_webpage\data1\img_gradient"
+        self.ROOT_IMG_CLEAN = "E:\Mulong\Datasets\dataset_webpage\data1\img_clean"
+        self.ROOT_IMG_SEGMENT = "E:\Mulong\Datasets\dataset_webpage\data1\img_segment"
+
+        self.THRESHOLD_MIN_OBJ_AREA = 200
+        self.THRESHOLD_MIN_REC_PARAMETER = 100
+        self.THRESHOLD_MIN_REC_EVENNESS = 0.8
+        self.THRESHOLD_MAX_EDGE_RATIO = 2.2
+        self.THRESHOLD_MAX_BORDER_THICKNESS = 5
+        self.THRESHOLD_MIN_LINE_THICKNESS = 10
+
+
+
+
---
+++ code/MODULE/img_processing_project/ip_main.py
@@ -0,0 +1,55 @@
+import ip_detection as det
+import ip_preprocessing as pre
+import ip_draw as draw
+import ip_segment as seg
+import file_utils as file
+from CONFIG import Config
+
+import cv2
+import time
+
+C = Config()
+
+is_save = True
+is_show = False
+
+start = time.clock()
+
+# pre-processing: gray, gradient, binary
+org, gray = pre.read_img('input/1.png', (0, 300))  # cut out partial img
+binary = pre.preprocess(gray, 1)
+
+# processing: get connected areas -> get boundary -> rectangle check -> get corner of boundaries -> img or frame check -> refine img component
+boundary_rec, boundary_all = det.boundary_detection(binary, C.THRESHOLD_MIN_OBJ_AREA, C.THRESHOLD_MIN_REC_PARAMETER, C.THRESHOLD_MIN_REC_EVENNESS, C.THRESHOLD_MIN_LINE_THICKNESS)
+corners_rec = det.get_corner(boundary_rec)
+corners_block, corners_img = det.block_or_img(binary, corners_rec, C.THRESHOLD_MAX_BORDER_THICKNESS)
+corners_img = det.img_refine2(corners_img, C.THRESHOLD_MAX_EDGE_RATIO)
+
+# remove img elements and segment into smaller size
+img_clean = draw.draw_bounding_box(corners_img, org, (255, 255, 255), -1)
+seg.segment_img(img_clean, 600, 'output/segment')
+
+# draw results
+draw_bounding = draw.draw_bounding_box(corners_block, org, (0, 255, 0))
+draw_bounding = draw.draw_bounding_box(corners_img, draw_bounding, (0, 0, 255))
+draw_boundary = draw.draw_boundary(boundary_rec, org.shape)
+
+# save results
+if is_save:
+    cv2.imwrite('output/org.png', org)
+    cv2.imwrite('output/labeled.png', draw_bounding)
+    cv2.imwrite('output/boundary.png', draw_boundary)
+    cv2.imwrite('output/gradient.png', binary)
+    cv2.imwrite('output/clean.png', img_clean)
+    file.save_corners('output/corners.csv', corners_block, 'div')
+    file.save_corners('output/corners.csv', corners_img, 'img', False)
+    
+# show results
+if is_show:
+    cv2.imshow('org', draw_bounding)
+    cv2.imshow('boundary', draw_boundary)
+    cv2.imshow('gradient', binary)
+    cv2.imshow('clean', img_clean)
+    cv2.waitKey(0)
+
+end = file.timer(start)
