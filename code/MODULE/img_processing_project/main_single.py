f3903c9248032ea5d59ed903a676418d55792105
Mulong <dsh15325@163.com>
2019-08-08 16:24:54 +1000

    add comments

--- code/MODULE/img_processing_project/main_single.py
+++ code/MODULE/img_processing_project/main_single.py
@@ -11,33 +11,32 @@ import time
 
 C = Config()
 input_root = C.ROOT_IMG_ORG
-
 is_save = True
 start = time.clock()
 
-# pre-processing: gray, gradient, binary
-org, gray = pre.read_img('input/1.png', (0, 3000))  # cut out partial img
+# *** Step 1 *** pre-processing: gray, gradient, binary
+org, gray = pre.read_img('input/10.png', (0, 3000))  # cut out partial img
 binary = pre.preprocess(gray, 1)
 
-# processing: get connected areas -> get boundary -> rectangle check -> get corner of boundaries ->
-# img or frame check -> refine img component
+
+# *** Step 2 *** processing: get connected areas -> get boundary -> rectangle check
 boundary_rec, boundary_all = det.boundary_detection(binary, C.THRESHOLD_MIN_OBJ_AREA,
                                                         C.THRESHOLD_MIN_REC_PARAMETER, C.THRESHOLD_MIN_REC_EVENNESS,
                                                         C.THRESHOLD_MIN_LINE_THICKNESS, C.THRESHOLD_MAX_IMG_DENT_RATIO)
+# get corner of boundaries -> img or block check
 corners_rec = det.get_corner(boundary_rec)
 corners_block, corners_img = det.block_or_img(binary, corners_rec, C.THRESHOLD_MAX_BLOCK_BORDER_THICKNESS, C.THRESHOLD_MAX_BLOCK_CROSS_POINT)
+# refine img component
 corners_img = det.img_refine2(corners_img, C.THRESHOLD_MAX_IMG_EDGE_RATIO, C.THRESHOLD_MIN_IMG_EDGE_LENGTH)
 
-# remove img elements and segment into smaller size
+
+# *** Step 3 *** post-processing: remove img elements from original image and segment into smaller size
 img_clean = draw.draw_bounding_box(corners_img, org, (255, 255, 255), -1)
 seg.segment_img(img_clean, 600, 'output/segment')
-
 # draw results
 draw_bounding = draw.draw_bounding_box(corners_block, org, (0, 255, 0))
 draw_bounding = draw.draw_bounding_box(corners_img, draw_bounding, (0, 0, 255))
-draw_boundary = draw.draw_boundary(boundary_all, org.shape)
-# draw_boundary = draw.draw_boundary(boundary_rec, org.shape)
-
+draw_boundary = draw.draw_boundary(boundary_rec, org.shape)
 # save results
 if is_save:
     cv2.imwrite('output/org.png', org)
@@ -48,4 +47,5 @@ if is_save:
     file.save_corners('output/corners.csv', corners_block, 'div')
     file.save_corners('output/corners.csv', corners_img, 'img', False)
 
+    open('output/blocks.txt', 'w').write(str(corners_block))
 end = file.timer(start)
