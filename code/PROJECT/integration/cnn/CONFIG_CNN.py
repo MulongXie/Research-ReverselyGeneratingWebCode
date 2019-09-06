381dd57d5ad8119ffd8722def995040e8e8989d7
Mulong <dsh15325@163.com>
2019-09-06 17:08:20 +1000

    update to latest cnn

--- code/PROJECT/integration/cnn/CONFIG_CNN.py
+++ code/PROJECT/integration/cnn/CONFIG_CNN.py
@@ -6,4 +6,4 @@ class Config:
         self.class_number = len(self.class_map)
 
         self.DATA_PATH = "E:/Mulong/Datasets/dataset_webpage/elements"
-        self.MODEL_PATH = 'E:/Mulong/Model/ui_compos/cnn3_img.h5'
+        self.MODEL_PATH = 'E:/Mulong/Model/ui_compos/cnn4_img.h5'
