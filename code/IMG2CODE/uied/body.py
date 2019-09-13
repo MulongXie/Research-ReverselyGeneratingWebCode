e1cfbbeaa990313f0a95c9399e2d0001442bc53d
Mulong <dsh15325@163.com>
2019-09-13 11:18:02 +1000

    seal up

--- code/IMG2CODE/uied/body.py
+++ code/IMG2CODE/uied/body.py
@@ -29,7 +29,7 @@ def pre_processing(input_path):
 def processing(org, binary, main=True):
     if main:
         # *** Step 2 *** object detection: get connected areas -> get boundary -> get corners
-        boundary_rec, boundary_non_rec = det.boundary_detection(binary, show=False)
+        boundary_rec, boundary_non_rec = det.boundary_detection(binary)
         corners_rec = det.get_corner(boundary_rec)
         corners_non_rec = det.get_corner(boundary_non_rec)
 
@@ -68,7 +68,7 @@ def processing(org, binary, main=True):
     # *** used for img inspection ***
     # only consider rectangular components
     else:
-        boundary_rec, boundary_non_rec = det.boundary_detection(binary, show=False)
+        boundary_rec, boundary_non_rec = det.boundary_detection(binary)
         corners_rec = det.get_corner(boundary_rec)
         corners_block, corners_img, corners_compo = det.block_or_compo(org, binary, corners_rec)
         compos = seg.clipping(org, corners_compo)
