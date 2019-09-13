cfbe47dc6e0d8dfd8d88b9f62190e1aab2303bc5
Mulong <dsh15325@163.com>
2019-09-13 21:31:03 +1000

    train cnn8 on Components2

--- code/PROJECT/model_cnn/lib/CONFIG.py
+++ code/PROJECT/model_cnn/lib/CONFIG.py
@@ -2,8 +2,8 @@
 class Config:
     def __init__(self):
         self.image_shape = (64, 64, 3)
-        self.class_map = ['button', 'input', 'icon', 'img']
+        self.class_map = ['button', 'input', 'icon', 'img', 'text']
         self.class_number = len(self.class_map)
 
-        self.DATA_PATH = "E:/Mulong/Datasets/dataset_webpage/elements"
-        self.MODEL_PATH = 'E:/Mulong/Model/ui_compos/cnn7_icon_no_text.h5'
+        self.DATA_PATH = "E:\Mulong\Datasets\Components2"
+        self.MODEL_PATH = 'E:/Mulong/Model/ui_compos/cnn8.h5'
