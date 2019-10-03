e500b74ba400cbceab18b486b5a6d6bc04e1d6ec
Mulong <dsh15325@163.com>
2019-10-03 18:32:09 +1000

    statistics of block from json

--- code/MODULE/plot/test.py
+++ code/MODULE/plot/test.py
@@ -2,6 +2,7 @@ from os.path import join as pjoin
 import os
 import json
 import glob
+import matplotlib.pyplot as plt
 
 label_root = 'E:\Mulong\Result\web\\ui_label'
 paths = glob.glob(pjoin(label_root, '*.json'))
@@ -18,3 +19,26 @@ for path in paths:
             heights.append(int(compo['height']))
             widths.append(int(compo['width']))
             ratios.append(int(compo['width']) / int(compo['height']))
+
+p = plt.figure(1)
+ax = plt.subplot(311)
+ax.set_title('Shape of Block')
+ax.set_xlabel('Height')
+ax.set_ylabel('Number')
+plt.grid(axis='y', ls='--')
+plt.hist(heights, bins=50, color='#99CCFF', range=(0,1000))
+
+ax = plt.subplot(312)
+ax.set_xlabel('Width')
+ax.set_ylabel('Number')
+plt.grid(axis='y', ls='--')
+plt.hist(widths, bins=50, color='#FFCC33', range=(0,1500))
+
+ax = plt.subplot(313)
+ax.set_xlabel('Width / Height')
+ax.set_ylabel('Number')
+plt.grid(axis='y', ls='--')
+plt.hist(ratios, bins=50, color='#0099CC', range=(0,10))
+
+plt.subplots_adjust(hspace=0.5)
+plt.show()
