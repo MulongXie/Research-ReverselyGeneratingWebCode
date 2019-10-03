3e9caca4d92c8ce7e25c9d4c6f1d6c38792e1a96
Mulong <dsh15325@163.com>
2019-10-03 19:44:32 +1000

    finalize

--- code/MODULE/plot/length_ratio.py
+++ code/MODULE/plot/length_ratio.py
@@ -7,7 +7,7 @@ root_button = 'E:\Mulong\Datasets\dataset_webpage\Components2\\button'
 root_input = 'E:\Mulong\Datasets\dataset_webpage\Components2\\input'
 root_img = 'E:\Mulong\Datasets\dataset_webpage\Components2\\img'
 
-paths = glob.glob(pjoin(root_img, '*'))
+paths = glob.glob(pjoin(root_button, '*'))
 
 heights = []
 widths = []
@@ -27,23 +27,23 @@ for i, path in enumerate(paths):
 
 p = plt.figure(1)
 ax = plt.subplot(311)
-ax.set_title('Shape of Image')
+ax.set_title('Shape of Button')
 ax.set_xlabel('Height')
 ax.set_ylabel('Number')
 plt.grid(axis='y', ls='--')
-plt.hist(heights, bins=50, color='#99CCFF', range=(0,1000))
+plt.hist(heights, bins=50, color='#99CCFF', range=(0,120))
 
 ax = plt.subplot(312)
 ax.set_xlabel('Width')
 ax.set_ylabel('Number')
 plt.grid(axis='y', ls='--')
-plt.hist(widths, bins=50, color='#FFCC33', range=(0,1500))
+plt.hist(widths, bins=50, color='#FFCC33', range=(0,700))
 
 ax = plt.subplot(313)
 ax.set_xlabel('Width / Height')
 ax.set_ylabel('Number')
 plt.grid(axis='y', ls='--')
-plt.hist(ratios, bins=50, color='#0099CC', range=(0,10))
+plt.hist(ratios, bins=50, color='#0099CC', range=(0,12))
 
 plt.subplots_adjust(hspace=0.5)
 plt.show()
