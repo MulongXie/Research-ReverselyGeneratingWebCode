7af6f1818ea25741641adfcc3942e57b8b3c2cb1
Mulong <dsh15325@163.com>
2019-11-22 13:23:42 +1100

    remove copy and relocate the clean code to a new repo

--- code/IMG2CODE - Copy/config/CONFIG_SVM.py
+++
@@ -1,9 +0,0 @@
-
-class Config:
-    def __init__(self):
-        self.image_shape = (64, 64, 3)
-        self.class_map = ['button', 'input', 'icon', 'img', 'text']
-        self.class_number = len(self.class_map)
-
-        self.DATA_PATH = "E:\Mulong\Datasets\Components3"
-        self.MODEL_PATH = 'E:/Mulong/Model/ui_compos/svm1.m'
