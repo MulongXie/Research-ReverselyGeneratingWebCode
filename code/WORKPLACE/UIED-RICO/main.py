1005d354b26bf24f3f415eb7c2f5ba4cbd529420
Mulong <dsh15325@163.com>
2020-01-29 17:53:06 +1100

    start simplifying the program

--- code/WORKPLACE/UIED-RICO/main.py
+++ code/WORKPLACE/UIED-RICO/main.py
@@ -5,6 +5,7 @@ import block_division as div
 import lib_uied.ip_preprocessing as pre
 import lib_uied.ip_segment as seg
 import lib_uied.ip_draw as draw
+import lib_uied.ip_detection_utils as util
 from lib_uied.MODEL_CNN import CNN
 import ui
 clf = CNN()
@@ -23,7 +24,7 @@ for i in range(len(blocks_corner)):
     bin = pre.preprocess(blocks_clip[i])
     corners_block, corners_img, corners_compo, compos_class = ui.processing(blocks_clip[i], bin, clf)
 
-    corners_img = div.block_rectify(blocks_corner[i], corners_img)
+    corners_img = util.corner_cvt_relative_position(corners_img, blocks_corner[i][0][0], blocks_corner[i][0][1])
 
     # draw_bounding = draw.draw_bounding_box_class(org, corners_compo, compos_class)
     # draw_bounding = draw.draw_bounding_box_class(draw_bounding, corners_block, ['block' for i in range(len(corners_block))])
