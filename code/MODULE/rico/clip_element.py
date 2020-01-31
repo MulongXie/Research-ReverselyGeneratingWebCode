fad23756a9ee4fb82e83d87915b8cf58740637c8
Mulong <dsh15325@163.com>
2020-01-31 11:30:12 +1100

    add exception control

--- code/MODULE/rico/clip_element.py
+++ code/MODULE/rico/clip_element.py
@@ -54,6 +54,8 @@ def fetch_and_clip(img, label, output_root, shrink_ratio=3, pad=False, show_labe
         y_min = min(int(l[1]), int(l[3]))
         x_max = max(int(l[0]), int(l[2]))
         y_max = max(int(l[1]), int(l[3]))
+        if y_max - y_min < 20 or x_max - x_min < 20:
+            continue
         element = element_map[l[4]]
         element_number[element] += 1
 
@@ -61,15 +63,15 @@ def fetch_and_clip(img, label, output_root, shrink_ratio=3, pad=False, show_labe
 
         if show_label:
             cv2.rectangle(img, (x_min, y_min), (x_max, y_max), (0, 0, 255), 1)
-            cv2.imshow('img', img)
+            cv2.imshow('img', shrink(img))
             cv2.waitKey(0)
 
 
 def read_files():
-    start_point = '14953'
+    start_point = '64366'
     locate = True
-
-    labels = open(ROOT_LABEL, 'r') 
+    bad_img = 0
+    labels = open(ROOT_LABEL, 'r')
     for i, l in enumerate(labels.readlines()):
         l = l.replace('./', ROOT_IMG).split()
         img_path = l[0]
@@ -86,7 +88,11 @@ def read_files():
         img = cv2.imread(img_path)
         img = cv2.resize(img, (1440, 2560))
         print(i, img_path)
-        fetch_and_clip(img, label, ROOT_OUTPUT)
+
+        try:
+            fetch_and_clip(img, label, ROOT_OUTPUT, show_label=False)
+        except:
+            print('*** Bad img:', bad_img, img_path)
 
 
 setup_folder()
